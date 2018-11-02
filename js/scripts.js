a878b73c9ce67e33042b1369967209c5fffd9061
katrina hockman <khockman90@hotmail.com>
2018-11-02 10:00:48 -0700

    Add length count for topping array

--- js/scripts.js
+++ js/scripts.js
@@ -1,41 +1,58 @@
-function Pizza(pizzaToppings, pizzaSize) {
-  this.pizzaToppings = pizzaToppings,
-  this.pizzaSize = pizzaSize,
-  this.pizzaPrice = // this.small, this.md, this. lrg??
+function Pizza() {
+  this.pizzaToppings = 0
+
+  //this.pizzaSize = pizzaSize,
+  //this.pizzaPrice = 0 // this.small, this.md, this. lrg??
 }
 
-Pizza.prototype.toppingQuantity = function() {
-  //toppings.forEach(function(count) {
-  //add $ to this.pizzaToppings
+Pizza.prototype.toppingPrice = function() {
+  toppingQty.length;
+      console.log(toppingQty.length);
 
-  })
-  //use submit-topping, and forEach loop to add $,
+     }
+  //  }
+  //   //return toppingCount + 1;
+  //
+  // }
+
+  //use submit-topping, and loop to add $,
   // push qty as pizzaToppings $$
-}
 
-Pizza.prototype.findPrice = function() {
+
+//Pizza.prototype.findPrice = function() {
   // use pizzaToppings $$,if toppings quantity = 1, price $, if topping qty = 2-4, price $$, topping qty 5+, price $$$
   // if size sm add $, md add $$, lrg add $$$
 
   // if sm add toppings... ad this.sm, etc
-}
-
-
-
-
+//}
+//
 
 //UI
 //submit topping button- from checkboxes= submit-topping
 // var toppings =[checkboxes];
 //
+var toppingQty = [];
+
+
 $(document).ready(function(){
+
   $("form#topping-select-1").submit(function(event){
     event.preventDefault();
-    $("#selected-toppings").show();
-    $("input:checkbox[name=pizza-topping]:checked").each(function(){
-      var selectedToppings = $(this).val();
-      $('#selected-toppings').append(selectedToppings + "<br>");
-    });
-    //$('#transportation_survey').hide();
-  });
-});
+      $("input:checkbox[name=pizza-topping]:checked").each(function() {
+      toppingQty.push($(this).val());
+      console.log(toppingQty);
+    })
+   var pizzaOrder = new Pizza();
+   //$("#selected-toppings").show();
+    // var selectedToppings = $(".topping:checked").val();
+
+     pizzaOrder.toppingPrice();
+
+    // $("#total").text(pizaOrder.pizzaPrice);
+    //
+
+
+     //$('#selected-toppings').append(selectedToppings + "<br>");
+    })
+  })
+//
