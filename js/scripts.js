c0b1c84131cfe88677e267d2a1fa35e22fa4c6a8
katrina hockman <khockman90@hotmail.com>
2018-11-02 10:26:27 -0700

    Add selector for size

--- js/scripts.js
+++ js/scripts.js
@@ -1,54 +1,50 @@
 function Pizza() {
   this.pizzaToppings = 0
-
-  //this.pizzaSize = pizzaSize,
-  //this.pizzaPrice = 0 // this.small, this.md, this. lrg??
+  this.pizzaSize = pizzaSize,
+  this.pizzaPrice = 0 // this.small, this.md, this. lrg??
 }
 
-Pizza.prototype.toppingPrice = function() {
+Pizza.prototype.findToppings = function() {
   toppingQty.length;
+  this.pizzaToppings += toppingQty.length;
       console.log(toppingQty.length);
+      console.log(this.pizzaToppings);
 
-     }
-  //  }
-  //   //return toppingCount + 1;
-  //
-  // }
+   }
 
-  //use submit-topping, and loop to add $,
-  // push qty as pizzaToppings $$
-
-
-//Pizza.prototype.findPrice = function() {
-  // use pizzaToppings $$,if toppings quantity = 1, price $, if topping qty = 2-4, price $$, topping qty 5+, price $$$
-  // if size sm add $, md add $$, lrg add $$$
-
-  // if sm add toppings... ad this.sm, etc
-//}
+// Pizza.prototype.findPrice = function() {
+//   use pizzaToppings $$,if toppings quantity = 1, price $, if topping qty = 2-4, price $$, topping qty 5+, price $$$
+//   if size sm add $, md add $$, lrg add $$$
 //
+//   if sm add toppings... ad this.sm, etc
+// }
+
 
 //UI
-//submit topping button- from checkboxes= submit-topping
-// var toppings =[checkboxes];
-//
-var toppingQty = [];
 
+var toppingQty = [];
+var size = [];
 
 $(document).ready(function(){
 
-  $("form#topping-select-1").submit(function(event){
+  $("form#pizza-order").submit(function(event){
     event.preventDefault();
+    //var sizeSelect = $("#size-select").val();
+    size.push($("#size-select").val());
+    console.log(size);
       $("input:checkbox[name=pizza-topping]:checked").each(function() {
       toppingQty.push($(this).val());
       console.log(toppingQty);
     })
+  //   $("input:checkbox[name=pizza-size]:checked").each(function() {
+  //   size.push($(this).val());
+  //   console.log(size);
+  // })
    var pizzaOrder = new Pizza();
-   //$("#selected-toppings").show();
-    // var selectedToppings = $(".topping:checked").val();
 
-     pizzaOrder.toppingPrice();
+     pizzaOrder.findPrice();
 
-    // $("#total").text(pizaOrder.pizzaPrice);
+     $("#total").text(pizaOrder.pizzaPrice);
     //
 
 
