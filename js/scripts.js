4c33b5ad4127a0e715fb7afe7113bae812167dcc
katrina hockman <khockman90@hotmail.com>
2018-11-02 12:02:39 -0700

    Add funtion to create multiple pizza order

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,48 @@
+//Create Order
+function PizzaOrder() {
+  this.pizzas = []
+  console.log(this.pizzas);
+}
+
+PizzaOrder.prototype.addPizza = function(pizza) {
+  pizza.id = this.assignId();
+  this.pizzas.push(pizza);
+}
+
+PizzaOrder.prototype.assignId = function() {
+  this.currentId +=1;
+  return this.currentId;
+}
+
+PizzaOrder.prototype.selectPizza = function(id) {
+  for (var i=0; i<this.pizzas.length; i++) {
+    if (this.pizzas[i]) {
+      if (this.pizzas[i].id == id) {
+        return this.pizzas[i];
+      }
+    }
+  };
+  return false;
+}
+PizzaOrder.prototype.deletePizza= function(id) {
+  for (var i=0; i< this.contacts.length; i++){
+    if (this.contacts[i]) {
+      if (this.contacts[i].id == id) {
+        delete this.contacts[i];
+        return true;
+      }
+    }
+  };
+  return false;
+}
+//End Create Order
+
+//Create Pizza
 function Pizza() {
+  this.pizzaName = pizzaName, //user assigns number/name to pizza for multiples order
   this.pizzaToppings = 0,
   this.pizzaSize = 0,
-  this.pizzaPrice = 0 // this.small, this.md, this. lrg??
+  this.pizzaPrice = 0
 }
 
 Pizza.prototype.findToppings = function() {
@@ -12,45 +53,78 @@ Pizza.prototype.findToppings = function() {
 
    }
 
-   Pizza.prototype.findSize = function() {
-     this.pizzaSize += parseInt(size);
-     console.log(this.pizzaSize);
-    }
+Pizza.prototype.findSize = function() {
+  this.pizzaSize += parseInt(size);
+    console.log(this.pizzaSize);
+  }
 
 Pizza.prototype.findPrice = function() {
   this.pizzaPrice += (this.pizzaToppings + this.pizzaSize)
   console.log(this.pizzaPrice)
 }
-
+//End Create Pizza
 
 //UI
-
+var newOrder = new PizzaOrder();
 var toppingQty = [];
 var size = [];
+var pizzaName = "";
+var pizzaInputName = $(".pizzaName").val();
+$("#pizzaName").text(pizzaInputName);
+
+function displayPizzaDetails(newOrderDisplay) {
+  var pizzaList = $("ul#pizzas");
+  var htmlForPizzaInfo = "";
+  newOrderDisplay.pizzas.forEach(function(pizza) {
+    htmlForPizzaInfo += "<li id=" + pizza.id + ">" + contact.pizzaName + "</li>";
+  });
+  pizzaList.html(htmlForPizzaInfo)
+};
+
+function showContact (pizzaId) {
+  var pizzaDetail = newOrder.selectPizza(pizzaId);
+  $("#show-detail").show();
+  $(".name").html(pizza.pizzaName);
+  var buttons = $("#buttons");
+  buttons.empty();
+  buttons.append("<button class='deleteButton' id=" +  + pizza.id + ">Delete</button>");
+}
+function attachContactListeners() {
+  $("ul#pizzas").on("click", "li", function(){
+    selectPizza(this.id);
+  });
+  $("#buttons").on("click", ".deleteButton", function(){
+    newOrder.deletePizza(this.id);
+    $("#show-detail").hide();
+    displayPizzaDetails(newOrder);
+  });
+};
+
+
 
 $(document).ready(function(){
+  attachContactListeners();
 
   $("form#pizza-order").submit(function(event){
     event.preventDefault();
-    //var sizeSelect = $("#size-select").val();
     size.push($("#size-select").val());
     console.log(size);
-      $("input:checkbox[name=pizza-topping]:checked").each(function() {
+    $("input:checkbox[name=pizza-topping]:checked").each(function() {
       toppingQty.push($(this).val());
-
     })
 
-   var pizzaOrder = new Pizza();
-     pizzaOrder.findToppings();
-     pizzaOrder.findSize();
-     pizzaOrder.findPrice();
+   var newPizza = new Pizza();
+     newPizza.findToppings();
+     newPizza.findSize();
+     newPizza.findPrice();
      $("#size-select option:selected").text();
-     console.log($("#size-select option:selected").text());
+     newOrder.addPizza(newPizza);
+     console.log(newOrder.pizzas);
 
-    // $("#price").text(pizzaOrder.pizzaPrice);
-     $('#selected-toppings').append(toppingQty + "<br>");
-    $('#selected-size').append($("#size-select option:selected").text() + "<br>");
-     $('#price').append("$" + pizzaOrder.pizzaPrice + "<br>");
+    // // $("#price").text(newPizza.pizzaPrice);
+    //  $('#selected-toppings').append(toppingQty + "<br>");
+    // $('#selected-size').append($("#size-select option:selected").text() + "<br>");
+    //  $('#price').append("$" + newPizza.pizzaPrice + "<br>");
     })
   })
 //
