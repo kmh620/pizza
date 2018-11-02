9921cf9625e995ae4ce442c5348227662467922f
katrina hockman <khockman90@hotmail.com>
2018-11-02 08:30:43 -0700

    Add initial files, add notes to js for plan

---
+++ README.md
@@ -0,0 +1,27 @@
+## **[title]**
+
+#### [Brief description], Date
+
+#### by _Katrina Hockman_
+
+### Description
+
+### Setup/Installation
+
+* _Log in to GitHub_
+* _Clone GitHub URL in terminal_
+* _Open the html file in your Web Browser_
+* _Open file in Atom_
+*  _Go to [{title} gh-pages link](https://kmh620.github.io/[repository-name]/)_
+
+
+
+
+### Technologies Used
+
+_GitHub, HTML, CSS, Bootstrap, Javascript, JQuery_
+
+
+
+#### license
+##### Copyright (c) 2018 *Katrina Hockman* MIT license
---
+++ js/scripts.js
@@ -0,0 +1,24 @@
+function Pizza(pizzaToppings, pizzaSize) {
+  this.pizzaToppings = pizzaToppings,
+  this.pizzaSize = pizzaSize,
+  this.pizzaPrice = // this.small, this.md, this. lrg??
+}
+
+Pizza.prototype.toppingQuantity = function() {
+  //use submit-topping, and forEach loop to add $,
+  // push qty as pizzaToppings $$
+}
+
+Pizza.prototype.findPrice = function() {
+  // use pizzaToppings $$,if toppings quantity = 1, price $, if topping qty = 2-4, price $$, topping qty 5+, price $$$
+  // if size sm add $, md add $$, lrg add $$$
+
+  // if sm add toppings... ad this.sm, etc
+}
+
+
+
+
+
+//UI
+//submit topping button- from checkboxes= submit-topping
